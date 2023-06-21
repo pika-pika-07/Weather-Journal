c211f4f20bffa2b2213007bc8bdff30abf936480
parth bhatnagar <parthbhatnagar@192.168.1.8>
2023-06-21 10:45:49 +0530

    Revert "Initial commit"
    This reverts commit 9f342b3b5628f6ed1e2b99a51353504ef6b007e7.

--- src/client/index.js
+++
@@ -1,8 +0,0 @@
-import { fetchWeatherData } from './js/app.js'
-import './styles/styles.scss'
-
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-    document.querySelector('form').addEventListener('submit', handleSubmit)
-});
